1996214ededef564ff6fcc7741b2ac3ab1dd57cc
Korten, Lukas <Lukas.Korten@adesso.de>
2020-07-28 06:10:06 +0200

    Initialer Commit für das Logging Beispiel

---
+++ spring-boot/progress-log/src/main/java/de/codesnacks/progresslog/ProgressLogApplication.java
@@ -0,0 +1,13 @@
+package de.codesnacks.progresslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressLogApplication.class, args);
+	}
+
+}
---
+++ spring-boot/progress-log/src/test/java/de/codesnacks/progresslog/ProgressLogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codesnacks.progresslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
