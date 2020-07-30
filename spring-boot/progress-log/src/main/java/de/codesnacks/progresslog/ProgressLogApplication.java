8ba30b37399fd32a90b00ca9bf0b40f2d6df184f
Korten, Lukas <Lukas.Korten@adesso.de>
2020-07-30 07:17:31 +0200

    Ein Beispiel zum Loggen eines Fortschritts implementiert

--- spring-boot/progress-log/src/main/java/de/codesnacks/progresslog/ProgressLogApplication.java
+++ spring-boot/progress-log/src/main/java/de/codesnacks/progresslog/ProgressLogApplication.java
@@ -1,13 +1,27 @@
 package de.codesnacks.progresslog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProgressLogApplication {
 
-	public static void main(String[] args) {
+	private static final Logger LOG = LoggerFactory.getLogger(ProgressLogApplication.class);
+
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(ProgressLogApplication.class, args);
+
+		LOG.info("start process ...");
+		Marker progress = MarkerFactory.getMarker("PROGRESS");
+		for (int i = 0; i <= 100; i++) {
+			LOG.info(progress, "items processed - {} / {}", i, 100);
+			Thread.sleep(100);
+		}
+		LOG.info("finish process ...");
 	}
 
 }
